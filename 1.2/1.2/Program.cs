09912d2d88d4cdac3dabc76efd0a6e5db08e8c13
Jesse14865 <45033406+Jesse14865@users.noreply.github.com>
2019-02-26 14:35:16 +0100

    Add files via upload

---
+++ 1.2/1.2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _1._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("********************");
+            Console.ReadLine();
+            Console.WriteLine("***     Jesse   ***");
+            Console.ReadLine();
+            Console.WriteLine("********************");
+            Console.ReadLine();
+        }
+    }
+}
